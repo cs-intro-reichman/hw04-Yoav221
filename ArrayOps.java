d772a334951071412d57e792264328b05cf3fc8d
Yoav Damari <yoav.damari@runi.ac.il>
2024-01-09 15:17:37 +0200

    finished second to max function

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,24 +1,37 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
-    }
-    
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int[] array = { 3, 0, 1 };
+        // System.out.println("the sorted array is: " + findMissingInt(array));
+
     }
 
-    public static int secondMaxValue(int [] array) {
-        // Write your code here:
+    public static int findMissingInt(int[] array) {
         return 0;
+
     }
 
-    public static boolean containsTheSameElements(int [] array1,int [] array2) {
+    public static int secondMaxValue(int[] array) {
         // Write your code here:
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        int SecondMax = 0;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > SecondMax && array[j] < max) {
+                SecondMax = array[j];
+            }
+        }
+        return SecondMax;
+    }
+
+    public static boolean containsTheSameElements(int[] array1, int[] array2) {
         return false;
     }
 
-    public static boolean isSorted(int [] array) {
+    public static boolean isSorted(int[] array) {
         // Write your code here:
         return false;
     }
