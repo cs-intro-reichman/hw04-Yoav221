9d2442387a758b3e70a6da1228dd59552520db74
Yoav Damari <yoav.damari@runi.ac.il>
2024-01-10 16:51:44 +0200

    almost finished stringops

--- StringOps.java
+++ StringOps.java
@@ -1,42 +1,76 @@
+import java.util.Arrays;
+
 public class StringOps {
     ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
+    ////// ///////
+    ////// Reminder: ///////
+    ////// allowed methods ///////
+    ////// ///////
+    ////// 1.charAt(int index) ///////
+    ////// 2.length() ///////
+    ////// 3.substring(int start) ///////
+    ////// 4.substring(int start,int ends) ///////
+    ////// 5.indexOf(String str) ///////
+    ////// ///////
+    ////// The rest are not allowed ! ///////
+    ////// if you want to use a different ///////
+    ////// method, and you can implement ///////
+    ////// it using material from the course ///////
+    ////// you need to implement a version of ///////
+    ////// the function by yourself. ///////
+    ////// ///////
+    ////// see example for substring ///////
+    ////// in Recitation 3 question 5 ///////
+    ////// ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        int[] indices = allIndexOf("Hello", 'l');
+        System.out.println(Arrays.toString(indices));
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String capVowelsLowRest(String string) {
+        String NewString = "";
+        for (int i = 0; i < string.length(); i++) {
+            char chr = string.charAt(i);
+            if (chr == 'a' || chr == 'e' || chr == 'i' || chr == 'o' || chr == 'u') {
+                NewString = NewString + ((char) (string.charAt(i) - 32));
+            } else if (chr == 'A' || chr == 'E' || chr == 'I' || chr == 'O' || chr == 'U') {
+                NewString = NewString + string.charAt(i);
+            } else if (chr >= 65 && chr <= 90) {
+                NewString = NewString + ((char) (string.charAt(i) + 32));
+            } else {
+                NewString = NewString + chr;
+            }
+
+        }
+        return NewString;
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String camelCase(String string) {
+        String result = "";
+        for (int i = 0; i < string.length(); i++) {
+
+        }
+        return result;
     }
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    public static int[] allIndexOf(String string, char chr) {
+        int l = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                l++;
+            }
+        }
+
+        int[] arr = new int[l];
+
+        int counter = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == chr) {
+                arr[counter] = i;
+                counter++;
+            }
+        }
+        return arr;
     }
 }
